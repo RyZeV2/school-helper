d1ccfee63805af37f806bdcc9ba6d06ea5216b3d
RyZeV2 <bcity3353@gmail.com>
2026-04-21 23:16:32 +0200

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,15 @@
-const CACHE_NAME = "schoolsaaS-v1";
+const CACHE = "school-saas-v1";
 
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c =>
+      c.addAll(["./", "./index.html", "./manifest.json"])
+    )
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
